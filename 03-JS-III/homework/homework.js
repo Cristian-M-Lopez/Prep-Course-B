e57e917541ba79c3ea65e8d150f73ca83acad16d
Cristian Lopez <weemileltd@gmail.com>
2022-10-17 16:42:19 -0300

    actualizacion

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,14 +8,13 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
-    // var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    // var suma = 0;
+    // var acumulador = 0;
 
-    // for (let i= 0; i < numeros.length; i++) {
-    //   suma += numeros[i];
-    // } return suma;
+    // for (let i= 1; i <= 11; i++) {
+    //   acumulador= acumulador + i;
+    // } return acumulador;
 
-    var total = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].reduce((a, b) => a + b);
+    var total = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].reduce((acumulador, num) => acumulador + num);
     return total;
 } 
 
@@ -23,14 +22,24 @@ function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
-  var encontrados = [];
+//otra forma de resolverlo (WHILE):
+// var pares = [];
+// var i = 0;
+// while (i < array.length) { //el indice deber ser menor que el tamanio del array, para que lo recorra.
+//   if (array[i] % 2 === 0) {
+//     pares.push(array[i]);
+// }  i++;
+// }  return pares;
+
+
+  var pares = [];
 
   for (let i = 0; i < array.length; i++) {
     if (array[i] % 2 === 0) {
-        encontrados.push(array[i]);
+        pares.push(array[i]);
     }
   } 
-  return encontrados;
+  return pares;
 
  
 }
@@ -39,8 +48,9 @@ function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
-  // var elevados = array.map(function(elemento){
-  //   return Math.pow(elemento,2);
+  // RESUELTO CON MAP
+  // return array.map(function(num){
+  //   return Math.pow(num,2);
   // });
 
   var elevados = [];
@@ -65,6 +75,10 @@ function sumaArray(array){
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  // otra forma num+a -1
+  // var numero = num + "";
+  // return numero;
+
   return num.toString().length;
 
 }
